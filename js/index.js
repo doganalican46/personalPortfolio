2d295673e62490273545fca4d9cbfb3d2c3565fc
Ali Can Dogan <76850300+doganalican46@users.noreply.github.com>
2023-07-25 13:23:09 +0300

    changes on css of email section

--- js/index.js
+++ js/index.js
@@ -2,6 +2,7 @@ function sendMail() {
   var params = {
     name: document.getElementById("name").value,
     email: document.getElementById("email").value,
+    subject: document.getElementById("subject").value,
     message: document.getElementById("message").value,
   };
 
@@ -12,9 +13,10 @@ function sendMail() {
     .then(res=>{
         document.getElementById("name").value = "";
         document.getElementById("email").value = "";
+        document.getElementById("subject").value = "";
         document.getElementById("message").value = "";
         console.log(res);
-        alert("Your message sent successfully!!")
+        alert("Your message sent successfully! I will response your message as soon as possible... Thanks for your message :) ")
 
     })
     .catch(err=>console.log(err));
